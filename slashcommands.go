0c95b55c3ac1cca75b0c4a79bb48bce5b7ef0c2e
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-09-04 01:14:59 -0400

    time parsing functions

--- slashcommands.go
+++ slashcommands.go
@@ -2,6 +2,10 @@ package main
 
 import (
     "fmt"
+    "strconv"
+    "regexp"
+    "time"
+    "errors"
     "github.com/bwmarrin/discordgo"
 )
 
@@ -21,6 +25,25 @@ var SlashCommands = []*discordgo.ApplicationCommand{
         Type: discordgo.ChatApplicationCommand,
         Description: "Nukes the server; used for development",
     },
+    {
+        Name: "new-assignment",
+        Type: discordgo.ChatApplicationCommand,
+        Description: "Creates a new assignment",
+        Options: []*discordgo.ApplicationCommandOption{
+            {
+                Type: discordgo.ApplicationCommandOptionString,
+                Name: "assignment-name",
+                Description: "name of the new assignment",
+                Required: true,
+            },
+            {
+                Type: discordgo.ApplicationCommandOptionString,
+                Name: "due-date",
+                Description: "date the assignment is due in YYYY-MM-DD HH:MM format",
+                Required: true,
+            },
+        },
+    },
 }
 
 var SlashCommandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
@@ -33,6 +56,7 @@ var SlashCommandHandlers = map[string]func(s *discordgo.Session, i *discordgo.In
         })
     },
     "init": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+        if !adminCommandPrivledge(s, i) { return }
 
         guildID := i.Interaction.GuildID
 
@@ -76,6 +100,13 @@ var SlashCommandHandlers = map[string]func(s *discordgo.Session, i *discordgo.In
 
         interactionSuccess("purged server", s, i)
     },
+    "new-assignment": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+        /* do some validation on args */
+        parseDateStringCommandArg(i.ApplicationCommandData().Options[1].StringValue())
+
+        /* insert assignment into database */
+
+    },
 }
 
 /* helpers */
@@ -85,6 +116,28 @@ func adminCommandPrivledge(s *discordgo.Session, i *discordgo.InteractionCreate)
     if (i.Interaction.Member.Permissions >> 3) & 0x1 == 0x1 { return true }
     interactionError("You do not have permission to execute this command.", s, i)
     return false
+}
+
+/* checks if string is in YYYY-MM-DD HH:MM format */
+/* converts datestring option to date */
+func parseDateStringCommandArg(dateString string) (*time.Time, error) {
+    /* issues with the regex - it's crude since its only for user friendliness */
+    /* doesnt check leap years */
+    /* doesnt validate number of days in each month */
+    /* doesnt check for zero month and dates ie 2021-00-00 */
+
+    r := regexp.MustCompile(`(\d{4})-(0\d|1[012])-([012]\d|3[01]) ([01]\d|2[0123]):([012345]\d)`)
+    matches := r.FindStringSubmatch(dateString)
+    if matches == nil { return nil, errors.New("invalid datestring") }
+
+    /* make this less horrendous */
+    year, _ := strconv.Atoi(matches[1])
+    month, _ := strconv.Atoi(matches[2])
+    day, _ := strconv.Atoi(matches[3])
+    hour, _ := strconv.Atoi(matches[4])
+    minute, _ := strconv.Atoi(matches[5])
+    t := time.Date(year, time.Month(month), day, hour, minute, 0, 0, time.UTC)
+    return &t, nil
 }
 
 /* interaction responses */
