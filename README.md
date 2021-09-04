0e8d7d9380a973827c3db89e2221e99545926352
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-09-03 21:19:32 -0400

    private channels for users

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 This bot requires not only the bot scope but also the applications.commands scope
 
+Invite bot using this url:
+```
+https://discord.com/api/oauth2/authorize?client_id=883409794319396866&permissions=8&scope=bot%20applications.commands
+```
+
--- handlers.go
+++ handlers.go
@@ -5,9 +5,9 @@ import (
 )
 
 /* main handler for slash commands */
-func SlashCommandHandler(s * discordgo.Session, i *discordgo.InteractionCreate) {
-    if handler, ok := SlashCommandHandlers[i.ApplicationCommandData().Name]; ok {
-        handler(s, i)
+func SlashCommandHandler(s *discordgo.Session, m *discordgo.InteractionCreate) {
+    if handler, ok := SlashCommandHandlers[m.ApplicationCommandData().Name]; ok {
+        handler(s, m)
     }
 }
 
@@ -20,3 +20,36 @@ func MessageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
     }
 }
 
+func JoinHandler(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
+    guildID := m.Member.GuildID
+
+    /* create channels for student */
+    studentChannelPermissions := []*discordgo.PermissionOverwrite{
+        {
+            ID: guildID,
+            Type: discordgo.PermissionOverwriteTypeRole,
+            Allow: 0,
+            Deny: 1024, // disable @everyone from viewing channel
+        },
+        {
+            ID: m.User.ID,
+            Type: discordgo.PermissionOverwriteTypeMember,
+            Allow: 1024, // allow viewing
+            Deny: 0,
+        },
+    }
+
+    studentCategory, _ := s.GuildChannelCreate(guildID, fmt.Sprintf("%s's channels"), discordgo.ChannelTypeGuildCategory);
+    s.GuildChannelCreateComplex(guildID, discordgo.GuildChannelCreateData{
+        Name: "questions",
+        Type: discordgo.ChannelTypeGuildText,
+        ParentID: studentCategory.ID,
+        Topic: "Use this channel to ask instructors any private questions",
+        PermissionOverwrites: studentChannelPermissions,
+
+    // _, err := s.UserChannelCreateComplex(guildID, GuildChannelCreateData{
+    //     Name: "private",
+    //     Type: discordgo.ChannelTypeDM,
+    // });
+
+}
